01798031e038dd1929231f858410ad76ab51d65f
Imam Satria <imam.satria280897@gmail.com>
2019-05-04 15:21:02 +0700

    dd

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express     = require("express")
 var bodyParser  = require("body-parser")
-var sql 		= require("mssql");
+var sql 		= require("mssql")
+var methodOverride 	= require("method-override")
 var app         = express()
+
+// For hosting
 var port = process.env.PORT
+
+// Use below if run locally
 // var port = process.env.PORT | 1000
 
 // Create a configuration object for our Azure SQL connection parameters
@@ -18,42 +23,42 @@ var dbConfig = {
 }
 
 app.set("view engine", "ejs")
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(methodOverride("_method"))
 
 app.get('/', function (req, res) {
-	// var todos = [{"task" : "Minum", "deadline" : "13.00"},
-	// 			 {"task" : "Makan", "deadline" : "12.00"}];   		
-			 
+	
 	sql.connect(dbConfig, (err) => {		
 		if(err)
 			console.log(err);
 		else {
 			var request = new sql.Request();
-			request.query("select * from todo", (err, record) => {
+			request.query("select * from candidates", (err, record) => {
 				if(err)
 					console.log(err);
 				else {
-					res.render("index", {todos: record.recordset});
+					res.render("index", {candidates: record.recordset});
 					sql.close();
 					console.log(record.recordset);	
 				}	
 			});		
 		}
-	});		 
+    });		 
+    
+	// res.render("index");
 });
 
-app.post('/', function (req, res) {				
+app.put('/:id/:earned_vote', function (req, res) {		
+	var vote = parseInt(req.params.earned_vote) + 1
+
 	sql.connect(dbConfig, (err) => {		
 		if(err)
 			console.log(err);
-		else {
-			var date = new Date().toISOString().split('T')[0];	
-			// console.log(date);		
+		else {			
 			var request = new sql.Request();
-			var query = "insert into todo (task, deadline, addedDate) values (" + 
-						"'" + req.body.task + "', " +
-						"'" + req.body.deadline + "', " +
-						"'" + date + "')";
+			var query = "update candidates set earned_vote= " + vote + " where id = " + req.params.id 						
+
 			// console.log(query);
 			request.query(query, (err, record) => {
 				if(err)
